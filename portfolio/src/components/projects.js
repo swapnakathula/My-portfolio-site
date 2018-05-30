68f57b7a8c523ab2cb5b190010fa739c66cfa310
swapnakathula <kathulaswapna@gmail.com>
2018-05-30 09:13:43 -0500

    projects page added

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,10 +1,85 @@
-import React,{ Component } from 'react';
-
+import React, { Component } from 'react';
+import { Grid,Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 
 class Projects extends Component {
-    render(){
-        return(
-            <div><h1>Projects Page</h1></div>
+    render() {
+        return (
+            <div className="projects-page">
+            <Grid className="demo-grid-1">
+               <Cell col={4}>
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                </Card>
+                </Cell>   
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+            </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                </Card>
+            
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+            </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                    <Button colored>Live Demo</Button>
+                        
+                    </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                </Card>
+                
+                <Card shadow={0} style={{ width: '350px', height: '320px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+                    </CardText>
+                    <CardActions border>
+                    <Button colored>Github</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                </Card>
+                
+                </Grid>
+            </div>
         );
     }
 }
